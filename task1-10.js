37e3f7d047dddcf8f4f09f49cab8da4f293f72c7
manisai66 <manikambalapalli@gmail.com>
2024-10-03 17:04:37 +0530

    Add files via upload

---
+++ task1-10.js
@@ -0,0 +1,120 @@
+var a="friday";
+switch (a) {
+    case "sunday":
+        console.log("yes this is sunday");
+        break;
+        case "monday":
+            console.log("yes this is monday");
+            break;
+            case "tuesday":
+                console.log("yes this is tuesday");
+                break;
+                case "wednesday":
+                    console.log("yes this is wedenesday");
+                    break;
+                    case "thursday":
+                        console.log("yes this is thursday");
+                        break;
+                        case "friday":
+                            console.log("yes this is friday man");
+                            break;
+                            case "saturday":
+                                console.log("yes this is saturday");
+                                break;
+                                default:
+                                    console.log("not there in a week");
+}
+
+
+
+var b="august";
+switch(b){
+    case "january":
+        console.log("in a year first month is january");
+        break;
+        case "febrauary":
+            console.log("in a year second month is feb");
+            break;
+            case "march":
+                console.log("in a year third month is march");
+                break;
+                case "april":
+                    console.log("in a year fourty month is april");
+                    break;
+                    case "may":
+                        console.log("in a year fifth month is may");
+                        break;
+                        case "june":
+                            console.log("in a year sixth month is june");
+                            break;
+                            case "july":
+                                console.log("in a year seventh month is july");
+                                break;
+                                case "august":
+                                    console.log("in a year eigth month is august");
+                                    break;
+                                    case "september":
+                                        console.log("in a year nineth month is september");
+                                        break;
+                                        case "october":
+                                            console.log("in a year tenth month is october");
+                                            break;
+                                            case "november":
+                                                console.log("in a year eleventh month is november");
+                                                break;
+                                                case "december":
+                                                    console.log("in a year month is december");
+                                                    break;
+                                                    default:
+                                                        console.log("in a year there are 12 months");
+}     
+
+
+var evenum=1;
+while(evenum<=10){
+    if(evenum%2==0){
+        console.log(evenum);
+   }
+   evenum++;
+
+    }
+
+
+
+    
+    
+    
+
+
+var count=10;
+while( count>=0  ){
+     
+        console.log(count);
+    
+        count--;
+}
+
+
+
+                                                
+                                            
+                                        
+                                        
+                                    
+                                
+                        
+
+                        
+                        
+                    
+                
+           
+            
+        
+
+    
+
+
+
+
+
